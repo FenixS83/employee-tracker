3b24aa7b0acebc24516b791ccbb73e4be6c97b1b
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-23 17:53:28 -0400

    added query for view all employees

--- employee_db.sql
+++ employee_db.sql
@@ -39,3 +39,6 @@ VALUES ("CEO", "100000", "7"), ("Software Developer", "70000", "2"), ("Lawyer", 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES ("Ryan", "Crowder", "1", "1"), ("Julian", "Pedraza", "2", "1"), ("Dauphine", "Burns", "3", "1"), ("Taegan", "Loyelle", "4", "1"), ("David", "Pedraza", "5", "1"), ("Jason", "Bailey", "6", "1"), ("Jordan", "Vasquez", "6", "1");
 
+--View All
+SELECT e.id, e.first_name, e.last_name, d.name AS department, r.title, r.salary, CONCAT_WS(" ", m.first_name, m.last_name) AS manager FROM employee e LEFT JOIN employee m ON m.id = e.manager_id INNER JOIN role r ON e.role_id = r.id INNER JOIN department d ON r.department_id = d.id ORDER BY e.id ASC;
+
