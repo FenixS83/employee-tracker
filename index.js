bd56c86b0c327ebbb3dc7043713297bf7973c5fa
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-23 19:27:16 -0400

    added Department adding section to index.js

--- index.js
+++ index.js
@@ -153,3 +153,27 @@ addSomething = () => {
             }
         });
 };
+
+//Department section of add menu
+addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: "department",
+                type: "input",
+                message: "What department would you like to add?",
+            },
+        ])
+
+        .then(function (answer) {
+            connection.query(
+                `INSERT INTO department (name) VALUES ('${answer.department}')`,
+                (err, res) => {
+                    if (err) throw err;
+                    console.log("1 new department added: " + answer.department);
+                    getDepartments();
+                    start();
+                }
+            );
+        });
+};
