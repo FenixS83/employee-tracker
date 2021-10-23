9a497c5224e0c14d5dca0fc20d506e6cb8cf2c84
Fenix Sampson <Fenix.sampson@gmail.com>
2021-10-23 18:58:13 -0400

    added initiating prompts to index.js

--- index.js
+++ index.js
@@ -54,3 +54,33 @@ connection.connect(function (err) {
     getEmployees();
 });
 
+// Initiating prompts
+start = () => {
+    inquirer
+        .prompt({
+            name: "choices",
+            type: "list",
+            message: "What wouuld you like to do?"
+            choices: ["ADD", "VIEW", "UPDATE", "DELETE", "EXIT"],
+        })
+        .then(function (answer) {
+            if (answer.choices === "ADD") {
+                addSomething();
+            } else if (answer.choices === "VIEW") {
+                viewSomething();
+            } else if (answer.choices === "UPDATE") {
+                updateSomething();
+            } else if (answer.choices === "DELETE") {
+                deleteSomething();
+            } else if (answer.choices === "EXIT") {
+                figlet("Thanks for using the Employee Tracker", (err, result) => {
+                    console.log(err || result);
+                });
+
+                connection.end();
+            } else {
+                connection.end();
+            }
+        });
+};
+
